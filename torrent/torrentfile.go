053f4ad72897424655000ba9cf23209cbbabdb42
Mohamed Elhefni <elhefnymohamed446@gmail.com>
2024-09-11 12:48:31 +0300

    chore: remove magnet link code into torrentfile

--- torrent/torrentfile.go
+++ torrent/torrentfile.go
@@ -3,9 +3,12 @@ package torrent
 import (
 	"bytes"
 	"crypto/sha1"
+	"encoding/base32"
 	"fmt"
-
 	"github.com/jackpal/bencode-go"
+	neturl "net/url"
+	"strconv"
+	"strings"
 )
 
 type bencodeInfo struct {
@@ -45,9 +48,34 @@ type bencodeTorrent struct {
 	Info     bencodeInfo `bencode:"info"`
 }
 
+// TODO: write implementation for magnet link to download the file info
 type BencodeMagnet struct {
 	InfoHash    string
 	DisplayName string
 	Length      int
 	TrackerURL  string
+}
+
+// magnet:?xt=urn:btih:DPIIR3URM2QGFT2K6COPTFZA7JXBUMJT&dn=debian-12.7.0-amd64-netinst.iso&xl=661651456&tr=http%3A%2F%2Fbttracker.debian.org%3A6969%2Fannounce
+func (bm *BencodeMagnet) Decode(url string) (TorrentFile, error) {
+	var file TorrentFile
+	colArr := strings.Split(url, ":")
+	hash := strings.Split(colArr[3], "&")[0]
+	encoded, err := base32.StdEncoding.DecodeString(hash)
+	if err != nil {
+		return file, err
+	}
+	file.InfoHash = string(encoded)
+	parsed, err := neturl.Parse(url)
+	if err != nil {
+		return file, err
+	}
+	file.DisplayName = parsed.Query().Get("dn")
+	fileLen, err := strconv.Atoi(parsed.Query().Get("xl"))
+	if err != nil {
+		return file, err
+	}
+	file.Length = fileLen
+	file.Announce = parsed.Query().Get("tr")
+	return file, nil
 }
